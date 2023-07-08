092ef2355456fbeda229dfa340bb5e6c374a7a22
sgorgun <sergei.gorgun@gmail.com>
2023-07-08 10:36:50 +0300

    Exam

--- GenericSequenceGenerator.Tests/FibonacciSequenceGeneratorTests.cs
+++ GenericSequenceGenerator.Tests/FibonacciSequenceGeneratorTests.cs
@@ -3,7 +3,7 @@
     [TestFixture]
     public class FibonacciSequenceGeneratorTests
     {
-        [TestCase(20, new int[]{ 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181 })]
+        [TestCase(20, new int[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181 })]
         public void GetNext_Should_Advance_Generator_In_Fibonacci_Sequence(int count, int[] expected)
         {
             var previous = 0;
@@ -22,6 +22,61 @@
             }
 
             CollectionAssert.AreEqual(expected, list.ToArray());
+            Assert.That(fibonacciGenerator.Count == count);
+        }
+    }
+
+    [TestFixture]
+    public class SecondSequenceGeneratorTests
+    {
+        [TestCase(10, new int[] { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512 })]
+        public void GetNext_Should_Advance_Generator_In_Fibonacci_Sequence(int count, int[] expected)
+        {
+            var previous = 1;
+            var current = 2;
+
+            var fibonacciGenerator = new SecondSequenceGenerator(previous, current);
+            var list = new List<int>(count)
+            {
+                fibonacciGenerator.Previous,
+                fibonacciGenerator.Current
+            };
+
+            for (int i = 1; i <= count - 2; i++)
+            {
+                list.Add(fibonacciGenerator.Next);
+            }
+
+            CollectionAssert.AreEqual(expected, list.ToArray());
+            Assert.That(fibonacciGenerator.Count == count);
+        }
+    }
+
+    public class ThirdSequenceGeneratorTests
+    {
+        [TestCase(10, new double[] { 1, 2, 2.5, 3.3, 4.05757575757576, 4.87086926018965, 5.70389834408211, 6.55785277425587, 7.42763417076325, 8.31053343902137 })]
+        public void GetNext_Should_Advance_Generator_In_Fibonacci_Sequence(int count, double[] expected)
+        {
+            var previous = 1;
+            var current = 2;
+
+            var fibonacciGenerator = new ThirdSequenceGenerator(previous, current);
+            var list = new List<double>(count)
+            {
+                fibonacciGenerator.Previous,
+                fibonacciGenerator.Current
+            };
+
+            for (int i = 1; i <= count - 2; i++)
+            {
+                list.Add(fibonacciGenerator.Next);
+            }
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.That(list[i], Is.EqualTo(expected[i]).Within(1e-10));
+            }
+
             Assert.That(fibonacciGenerator.Count == count);
         }
     }
---
+++ GenericSequenceGenerator/GenericSequenceGenerator.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace GenericSequenceGenerator;
+public interface ISequenceGenerator<out T>
+{
+    T Previous { get; }
+    T Current { get; }
+    T Next { get; }
+}
+
+public abstract class SequenceGenerator<T> : ISequenceGenerator<T>
+{
+    protected T previous;
+    protected T current;
+
+    public T Previous => previous;
+    public T Current => current;
+    public T Next
+    {
+        get => GetNext();
+        set => throw new NotImplementedException();
+    }
+    public int Count { get; set; }
+
+    protected SequenceGenerator(T previous, T current)
+    {
+        this.previous = previous;
+        this.current = current;
+        Count = 2;
+    }
+
+    public abstract T GetNext();
+}
+
+public class FibonacciSequenceGenerator : SequenceGenerator<int>
+{
+    public FibonacciSequenceGenerator(int previous, int current) : base(previous, current)
+    {
+    }
+
+
+    public override int GetNext()
+    {
+        int next = previous + current;
+        previous = current;
+        current = next;
+        Count++;
+        return next;
+    }
+}
+
+public class SecondSequenceGenerator : SequenceGenerator<int>
+{
+    public SecondSequenceGenerator(int previous, int current) : base(previous, current)
+    {
+    }
+
+    public override int GetNext()
+    {
+        var next = 6 * current - 8 * previous;
+        previous = current;
+        current = next;
+        Count++;
+        return next;
+    }   
+}
+
+public class ThirdSequenceGenerator : SequenceGenerator<double>
+{
+    public ThirdSequenceGenerator(double previous, double current) : base(previous, current)
+    {
+    }
+
+    public override double GetNext()
+    {
+        var next = current + previous / current;
+        previous = current;
+        current = next;
+        Count++;
+        return next;
+    }
+}
